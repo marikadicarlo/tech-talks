94bde04c1105b6ec1ad429c57c520d1f631fe605
marikadicarlo <mdicarlo19@yahoo.com>
2022-02-28 21:37:38 -0500

    added comment-routes.js

---
+++ routes/api/comment-routes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+router.get('/', (req, res) => {});
+
+router.post('/', (req, res) => {});
+
+router.delete('/:id', (req, res) => {});
+
+module.exports = router;
